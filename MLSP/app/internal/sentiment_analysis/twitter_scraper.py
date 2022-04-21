14f6dbf8acc13064cbac07bb9e5d1ea8267ee583
Glenn McKnight <gmckhjh@gmail.com>
2022-04-21 21:04:39 +0100

    Twitter Scraper refactored to solve import issue.
    Python library used for tweet cleaning with different import name to pip install name causing issues with deployment. Replaced with custom cleaning function.

--- MLSP/app/internal/sentiment_analysis/twitter_scraper.py
+++ MLSP/app/internal/sentiment_analysis/twitter_scraper.py
@@ -1,6 +1,7 @@
 from datetime import date as date, timedelta
 from typing import Optional
-import preprocessor as p
+import re
+from preprocessor import preprocess as p
 
 import snscrape.modules.twitter as scraper
 from fastapi import HTTPException
@@ -35,9 +36,8 @@ def scrape_hashtag(hashtag: str, date_start: Optional[date] = date.today() - tim
                 break
 
             # Clean and append tweet data
-            p.set_options(p.OPT.URL)
-            clean_tweet = p.clean(tweet.content)
-            curr_tweet = (tweet.date.strftime("%Y/%m/%d"), clean_tweet)
+            cleaned_tweet = clean_tweet(tweet.content)
+            curr_tweet = (tweet.date.strftime("%Y/%m/%d"), cleaned_tweet)
             scraped_tweets.append(curr_tweet)
 
         date_start += timedelta(days=1)
@@ -47,3 +47,14 @@ def scrape_hashtag(hashtag: str, date_start: Optional[date] = date.today() - tim
         raise HTTPException(status_code=404, detail="No tweets found")
 
     return scraped_tweets
+
+
+def clean_tweet(tweet):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+) | "
+                           "([^0-9A-Za-z \t]) | "
+                           "(\w+:\ / \ / \S+) | "
+                           "(#[A-Za-z0-9_]+) | "
+                           "(rhttp\S+) | "
+                           "(rwww.\S+) | "
+                           "([()!?,]) | "
+                           "(\[.*?\])", " ", tweet).split())
