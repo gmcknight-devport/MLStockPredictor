8093bebada4ddf1c70d671c0ae70ac4ca78411cf
Glenn McKnight <gmckhjh@gmail.com>
2022-04-22 00:50:46 +0100

    Update main_mlsp.yml
    Revert "Update main_mlsp.yml"
    
    This reverts commit bf652e6c4af56f600cc008412acde87e011c3235.
    
    Update startup.sh
    
    Update startup.sh
    
    Reqs and startup update
    
    Remove the Azure App Service build and deployment workflow config

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
-# python -m uvicorn main:app --host 0.0.0.0
+# python -m uvicorn main:app --host 0.0.0.0:8000
 # python -m gunicorn main:app -k uvicorn.workers.UvicornWorker
