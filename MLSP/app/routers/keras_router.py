2dc9b83df38b25be99e68fdaae99f59e63e6af90
Glenn McKnight <gmckhjh@gmail.com>
2022-04-26 01:45:57 +0100

    Update keras_router.py
    Error handling for api

--- MLSP/app/routers/keras_router.py
+++ MLSP/app/routers/keras_router.py
@@ -20,36 +20,41 @@ def predict_stock(ticker: Ticker, model_options: ModelOptions, model_name: Optio
                   activation: Optional[str] = "tanh", train_percentage: Optional[float] = 0.8,
                   time_step: Optional[int] = None):
 
-    # Check model exists in array:
-    if model_name not in models:
-        raise HTTPException(status_code=404, detail="Model couldn't be found")
+    try:
+        # Check model exists in array:
+        if model_name not in models:
+            raise HTTPException(status_code=404, detail="Model couldn't be found")
 
-    # If number if iterations is invalid (not between 1 and 10) raise exception
-    if not 1 <= model_options.iterations <= 10:
-        raise HTTPException(status_code=400, detail="Enter number of iterations between 1 and 10")
+        # If number if iterations is invalid (not between 1 and 10) raise exception
+        if not 1 <= model_options.iterations <= 10:
+            raise HTTPException(status_code=400, detail="Enter number of iterations between 1 and 10")
 
-    # Preprocessing
-    train_x, train_y, test_x, test_y, scale, time_step = get_processed_ticker_data(ticker.ticker, ticker.date_start,
-                                                                                   ticker.date_end, train_percentage,
-                                                                                   time_step)
+        # Preprocessing
+        train_x, train_y, test_x, test_y, scale, time_step = get_processed_ticker_data(ticker.ticker, ticker.date_start,
+                                                                                       ticker.date_end, train_percentage,
+                                                                                       time_step)
 
-    # Check activation function
-    if activation not in activation_functions:
-        model_options.activation = "tanh"
+        # Check activation function
+        if activation not in activation_functions:
+            model_options.activation = "tanh"
 
-    # Set input shape and call Keras model
-    input_shape = (time_step, 1)
-    test_predictions, test_y, future_predictions = create_model(model_name, model_options, input_shape, activation,
-                                                                scale, train_x, train_y, test_x, test_y)
+        # Set input shape and call Keras model
+        input_shape = (time_step, 1)
+        test_predictions, test_y, future_predictions = create_model(model_name, model_options, input_shape, activation,
+                                                                    scale, train_x, train_y, test_x, test_y)
 
-    # calculate accuracy
-    metrics = RegressionAccuracy.calc_accuracy(test_predictions[:, 0], test_y[0])
+        # calculate accuracy
+        metrics = RegressionAccuracy.calc_accuracy(test_predictions[:, 0], test_y[0])
 
-    # Prepare output for JSON encoding
-    test_predictions = dict(enumerate(test_predictions.flatten(), 1))
-    future_predictions = dict(enumerate(future_predictions.flatten(), 1))
+        # Prepare output for JSON encoding
+        test_predictions = dict(enumerate(test_predictions.flatten(), 1))
+        future_predictions = dict(enumerate(future_predictions.flatten(), 1))
 
-    for i in test_predictions:
-        test_predictions[i] = float(test_predictions[i])
+        for i in test_predictions:
+            test_predictions[i] = float(test_predictions[i])
 
-    return test_predictions, metrics, future_predictions
+        return test_predictions, metrics, future_predictions
+    
+    except HTTPException:
+        import traceback
+        return traceback.extract_stack()
